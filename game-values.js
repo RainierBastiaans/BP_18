069f8e0a5aeceaa1eb514cb8af569ba9528e502f
gregory <gregory.meyer@student.ucll.be>
2024-06-05 13:14:11 +0200

    scalable ui update

--- game-values.js
+++ game-values.js
@@ -31,10 +31,10 @@ export const gameValues = {
   jitExtraPrice: 1.5, // Shorter name for extrapriceJit
 
   // Round duration (seconds)
-  roundDuration: 5, // Shorter name for timeOfRound
+  roundDuration: 120, // Shorter name for timeOfRound
 
   // Parts added to stock per round
-  partsPerRound: 10,
+  partsPerRound: 20,
 
   //Number of rounds
   numberOfRounds: 5,
