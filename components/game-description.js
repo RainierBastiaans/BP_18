c1493db6b12da1d6dcff5bc219c98857bf524543
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2024-06-06 16:13:17 +0200

    temp

--- components/game-description.js
+++ components/game-description.js
@@ -4,18 +4,19 @@ class GameDescription extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="styles.css">
+            <h2 class = "game-title">Configure your factory!</h2>
             <p class = "game-description">
                 This is an exciting game where you can test your LEAN skills and challenge your friends.
             </p>
         `;
   }
-  show(){
-    this.classList.remove("hidden")
+  show() {
+    this.classList.remove("hidden");
   }
-  hide(){
-    this.classList.add("hidden")
+  hide() {
+    this.classList.add("hidden");
   }
 }
 
 customElements.define("game-description", GameDescription);
-export {GameDescription}
+export { GameDescription };
