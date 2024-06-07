53877bdd8b039f9b6a21f6b29662e0d1c0e27b7e
gregory <gregory.meyer@student.ucll.be>
2024-06-07 09:36:33 +0200

    color update

--- game-values.js
+++ game-values.js
@@ -35,7 +35,7 @@ export const gameValues = {
   jitExtraPrice: 1.5, // Shorter name for extrapriceJit
 
   // Round duration (seconds)
-  roundDuration: 10, // Shorter name for timeOfRound
+  roundDuration: 50, // Shorter name for timeOfRound
 
   // Parts added to stock per round
   partsPerRound: 10,
